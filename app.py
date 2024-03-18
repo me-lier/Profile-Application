3fd8c172bb2f713a60949d2afac22fe383bd00ab
Srinivas Deekonda <126309422+me-lier@users.noreply.github.com>
2024-03-18 23:01:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,12 +44,10 @@ def login():
 
 @app.route('/logout')
 def logout():
-
-
-session.pop('loggedin', None)
-session.pop('id', None)
-session.pop('username', None)
-return redirect(url_for('login'))
+	session.pop('loggedin', None)
+	session.pop('id', None)
+	session.pop('username', None)
+	return redirect(url_for('login'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
